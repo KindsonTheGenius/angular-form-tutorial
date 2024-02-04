61af15061b7b75bec70ca066c8f9b9c029c71dc0
KindsonTheGenius <Smart24*>
2024-02-04 11:02:31 +0100

    Performed ng update

--- src/app/r-form/r-form.component.spec.ts
+++ src/app/r-form/r-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RFormComponent } from './r-form.component';
 
@@ -6,7 +6,7 @@ describe('RFormComponent', () => {
   let component: RFormComponent;
   let fixture: ComponentFixture<RFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RFormComponent ]
     })
--- src/app/td-form/td-form.component.spec.ts
+++ src/app/td-form/td-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TdFormComponent } from './td-form.component';
 
@@ -6,7 +6,7 @@ describe('TdFormComponent', () => {
   let component: TdFormComponent;
   let fixture: ComponentFixture<TdFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TdFormComponent ]
     })
